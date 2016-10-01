5552ad5eff4a8c6fde87505623bcd3c0ddfdb90b
Annie Xie <axie0906@gmail.com>
2016-10-01 10:06:06 -0700

    revised 2048

--- 2048/2048script.js
+++ 2048/2048script.js
@@ -2,7 +2,7 @@
 	a. keep your gameboard as a 2D-array
 	b. render the 2D-array in the browser (using JS to edit the html - we will group code this!)
 	c. re-write the arrowPressed() function to update your 2D array gameboard when the player hits an arrow key
-	d. write your own 'helper' functions that break 2048 into smaller problems. 
+	d. write your own 'helper' functions that break 2048 into smaller problems.
 */
 
 /*CONSTANTS: These match keyCode values to arrow directions. do not edit, and do no assign values!*/
@@ -15,20 +15,20 @@ var DOWN = 40;
 
 
 //board as a 2D array with dummy values. This 2D array is the MODEL that remembers
-//what are gameboard (grid) contains. In the end it will start with all zeros. 
+//what are gameboard (grid) contains. In the end it will start with all zeros.
 var board = [["","","",""],
-			 ["","","",""],
+			 			 ["","","",""],
              ["","","",""],
              ["","","",""]];
 
-	
+
 /*Sample testboard: this has numbers hard-coded to test whether the UP arrow can handle 2s:
-var testTwosUpBoard = [[0,0,2,0], 
-			[0,0,2,0], 
-			[2,0,0,0], 
+var testTwosUpBoard = [[0,0,2,0],
+			[0,0,2,0],
+			[2,0,0,0],
 			[2,0,0,0]];*/
 
-//runGame() will call functions you write to solve the subProblems of 2048 the game.  
+//runGame() will call functions you write to solve the subProblems of 2048 the game.
 var start = function(){
 	var l = Math.floor(Math.random()*4);
 	var m = Math.floor(Math.random()*4);
@@ -76,9 +76,9 @@ var add2 = function(){
 
 function showBoard(){
 	start();
-	colorBoard();	
+	colorBoard();
 	renderBoard();
-	
+
 }
 
 var left = function() {
@@ -149,8 +149,8 @@ var down = function() {
 	renderBoard();
 }
 
-//arrowPress receives the arrow key pressed by the user. Right now all it does is print out the key they pressed. 
-//Starting with "UP," edit this code so that your game responds appropriately to the arrow keys.  
+//arrowPress receives the arrow key pressed by the user. Right now all it does is print out the key they pressed.
+//Starting with "UP," edit this code so that your game responds appropriately to the arrow keys.
 function arrowPress(e){
 	if(e.keyCode == UP){
 		console.log("UP");
@@ -180,18 +180,18 @@ function renderBoard(){
 			renderSquare(i,j);
 		}
 	}
-} 
+}
 
-/*You do not need to edit this function. It takes 2 parameters: the rowIndex and the colIndex. It 
+/*You do not need to edit this function. It takes 2 parameters: the rowIndex and the colIndex. It
 uses Javascript to update the html cell at rowIndex,colIndex with the element at board[rowIndex][colIndex]*/
 function renderSquare(rowIndex, colIndex){
-	var boardID = "r"+rowIndex+"c"+colIndex; 
+	var boardID = "r"+rowIndex+"c"+colIndex;
 	document.getElementById(boardID).innerHTML = board[rowIndex][colIndex];
 }
 
-/*colorBoard() loops over our 2D array to color the squares. It will eventually assign each cell a color depending on the number the cell holds. 
+/*colorBoard() loops over our 2D array to color the squares. It will eventually assign each cell a color depending on the number the cell holds.
 	Here you can see how javascript accesses pieces of HTML and edits their style (CSS) with new colors.
-	You do not need to edit this code until after your game works and you 
+	You do not need to edit this code until after your game works and you
 	want to mix up the colors., but it is a reference for how to do a double-for loop over the 2D-array.
 	*/
 function colorBoard(){
@@ -212,7 +212,7 @@ function colorBoard(){
 			if(board[i][j]===8){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#f59563";
-			}			
+			}
 			if(board[i][j]===16){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#3399ff";
@@ -228,7 +228,7 @@ function colorBoard(){
 			if(board[i][j]===128){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#800080";
-			}			
+			}
 			if(board[i][j]===256){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#990303";
@@ -236,7 +236,7 @@ function colorBoard(){
 			if(board[i][j]===512){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#6ba5de";
-			}			
+			}
 			if(board[i][j]===1024){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#dcad60";
@@ -244,10 +244,7 @@ function colorBoard(){
 			if(board[i][j]===2048){
 				var boardID = "r"+i+"c"+j; //build the ID string
 				document.getElementById(boardID).style.background = "#b60022";
-			}			
+			}
 		}
 	}
 }
-
-
-
